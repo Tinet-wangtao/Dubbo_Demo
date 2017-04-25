755048ce232a215dd361be83fc2a36371ce65cf2
Tinet-wangtao <wangtao@ti-net.com.cn>
2017-04-25 08:47:10 +0800

    搭建Dubbo + Zookeeper 本地服务的项目工程

---
+++ provider/src/main/java/com/wt/service/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.wt.service.impl;
+
+import com.wt.service.TestService;
+
+public class TestServiceImpl implements TestService {
+
+	public String getName() {
+		return "hohoTT";
+	}
+
+}
